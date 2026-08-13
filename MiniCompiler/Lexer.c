cae82ff2a41dc8b0683caf40f42788f9bf7d436d
Ilyas Ait Said <ilyasaitsaiddd@gmail.com>
2026-08-13 04:52:56 +0100

    features for lexer - Fix bugs & logic

--- MiniCompiler/Lexer.c
+++ MiniCompiler/Lexer.c
@@ -1,28 +1,118 @@
-#include <stdlib.h>
-typedef enum enum_type_token
+#include "library.h"
+type_token Tokenize_var_call(char *str, int *i)
 {
-    TOKEN_NUMBER,
-	TOKEN_PLUS,
-	TOKEN_MINUS,
-    TOKEN_ERROR
-} e_type_token;
-typedef struct s_type_token
+    type_token var_token;
+    char *base = "var";
+    char *var = malloc(4);
+    if (var == NULL)
+    {
+        var_token.type = TOKEN_ERROR;
+        var_token.str_value = NULL;
+        return var_token;
+    }
+    int j = 0;
+    while(*i < 3)
+    {
+        if (str[*i] == base[j]) 
+            var[j++] = str[*i];
+        else
+        {
+            var_token.type = TOKEN_ERROR;
+            var_token.str_value = NULL;
+            free(var);
+            return var_token;
+        }
+        (*i)++;
+    }
+    var_token.type = TOKEN_CALL_VAR;
+    var[j] = '\0';
+    var_token.str_value = var;
+    return var_token;
+}
+
+type_token Tokenize_var_type(char *str, int *i)
 {
-    e_type_token type;
-    int          value;
-}   type_token;
+    type_token type_token;
+    int start = *i;
+    int j = 0;
+    while (str[start] >= 'a' && str[start] <= 'z')
+        start++;
+    j = start - *i;
+    char *type = malloc(j + 1);
+    if (type == NULL)
+    {
+        type_token.type = TOKEN_ERROR;
+        type_token.str_value = NULL;
+        return type_token;
+    }
+    j = 0;
+    while (str[*i] != ' ' && str[*i])
+    {
+        if (str[*i] >= 'a' && str[*i] <= 'z')
+            type[j++] = str[*i];
+        else
+        {
+            type_token.type = TOKEN_ERROR;
+            type_token.str_value = NULL;
+            free(type);
+            return type_token; 
+        }
+        (*i)++;
+    }
+    type[j] = '\0';
+    type_token.type = TOKEN_TYPE_VAR;
+    type_token.str_value = type;
+    return type_token;
+}
+
+type_token Tokenize_id(char *str, int *i)
+{
+    type_token id_token;
+    int j = 0;
+    int start = *i;
+    while ((str[start] >= 'a' && str[start] <= 'z') || (str[start] >= 'A' && str[start] <= 'Z')
+    || str[start] == '_' || (str[start] >= '0' && str[start] <= '9'))
+        start++;
+    j = start - *i;
+    char *id_str = malloc(j + 1);
+    if (id_str == NULL)
+    {
+        id_token.type = TOKEN_ERROR;
+        id_token.str_value = NULL;
+        return id_token;
+    }
+    j = 0;
+    while (str[*i] != ' ' && str[*i])
+    {
+        if((str[*i] >= 'a' && str[*i] <= 'z') || (str[*i] >= 'A' && str[*i] <= 'Z')
+        || str[*i] == '_' || (str[*i] >= '0' && str[*i] <= '9'))
+            id_str[j++] = str[*i];
+        else
+        {
+            id_token.type = TOKEN_ERROR;
+            id_token.str_value = NULL;
+            free(id_str);
+            return id_token;
+        }
+        (*i)++;
+    }
+    id_str[j] = '\0';
+    id_token.type = TOKEN_IDENTIFIER;
+    id_token.str_value = id_str;
+    return id_token;
+}
+
 char *Tokenize_num(char *str, int *i)
 {
-    int size = 0;
+    int size = *i;
     int j = 0;
     while (str[size] >= '0' && str[size] <= '9')
         size++;
-    size = size - *i;
-    char *token = malloc(size + 1);
+    j = size - *i;
+    char *token = malloc(j + 1);
     if (token == NULL)
         return NULL;
-    while (str[*i] == ' ')
-        (*i)++;
+    j = 0;
     while (str[*i] >= '0' && str[*i] <= '9')
     {
         token[j++] = str[*i];
@@ -31,6 +121,7 @@ char *Tokenize_num(char *str, int *i)
     token[j] = '\0';
     return token;
 }
+
 type_token Str_To_Num(char *str, int *i)
 {
     int number = 0;
@@ -53,35 +144,70 @@ type_token Str_To_Num(char *str, int *i)
     free(tokenStr);
     return (token);
 }
-type_token Tokenize(char *str)
+void Skip_Space(char *str, int *i)
+{
+        while (str[*i] == ' ')
+	    (*i)++;
+}
+type_token *Tokenize(char *str)
 {
     int t = 0;
     int i = 0;
-    type_token	tokens[100];
+    type_token	*tokens;
+    tokens = malloc (sizeof(type_token) * 100);
+    if (tokens == NULL)
+        return NULL;
+    tokens[t] = Tokenize_var_call(str, &i);
+    t++;
+    Skip_Space(str, &i);
+    tokens[t] = Tokenize_var_type(str, &i);
+    t++;
+    Skip_Space(str, &i);
+    tokens[t] = Tokenize_id(str, &i);
+    t++;
+    Skip_Space(str, &i);
+    if(str[i] == '=')
+    {
+        tokens[t].type = TOKEN_EQUAL;
+        tokens[t].str_value = "=";
+        t++;
+        i++;
+    }
+    Skip_Space(str, &i);
     tokens[t] = Str_To_Num(str, &i);
     t++;
-    while (str[i] == ' ')
-	i++;
-    if(str[t] == '+')
+    Skip_Space(str, &i);
+    if(str[i] == '+')
     {
         tokens[t].type = TOKEN_PLUS;
+        tokens[t].str_value = "+";
         t++;
         i++;
     } 
-    else if(str[t] == '-')
+    else if(str[i] == '-')
     {
         tokens[t].type = TOKEN_MINUS;
+        tokens[t].str_value = "-";
         t++; 
         i++;
     }
+    Skip_Space(str, &i);
     tokens[t] = Str_To_Num(str, &i);
     t++;
-    return tokens[100];
+    if(str[i] == ';')
+    {
+        tokens[t].type = TOKEN_SEMICOLON;
+        tokens[t].str_value = ";";
+        t++;
+        i++;
+    }
+    return tokens;
 }
+
 int main ()
 {
-    char* Text = "15 + 1";
-    type_token	tokens[100];
-    tokens[100] = Tokenize(Text);
+    char* Text = "var int x = 1 + 5;";
+    type_token *tokens;
+    tokens = Tokenize(Text);
     return 0;
 }
